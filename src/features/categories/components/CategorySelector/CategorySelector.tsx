1ff82bc02ad4368ce88d92a356356e2e8527ea7e
Krztk <52879665+Krztk@users.noreply.github.com>
2022-07-29 20:00:06 +0200

    Add CategorySelector

---
+++ src/features/categories/components/CategorySelector/CategorySelector.tsx
@@ -0,0 +1,40 @@
+import { CategoryDTO } from "../../types";
+import { Controller, FieldValues, Path } from "react-hook-form";
+import { HookFormControlled } from "types";
+import { Box, MantineStyleSystemProps, MultiSelect } from "@mantine/core";
+import { mapToSelectEntry } from "../../mappers/categoryMappers";
+
+interface CategorySelectorProps<TFieldValues>
+  extends HookFormControlled<TFieldValues>,
+    MantineStyleSystemProps {
+  name: Path<TFieldValues>;
+  categories: CategoryDTO[];
+}
+
+export const CategorySelector = <TFieldValues extends FieldValues>({
+  name,
+  categories,
+  control,
+  ...mantineProps
+}: CategorySelectorProps<TFieldValues>) => {
+  return (
+    <Box {...mantineProps}>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field, fieldState: { error } }) => {
+          const entries = categories.map(mapToSelectEntry);
+          console.log("field value", field.value);
+          return (
+            <MultiSelect
+              label="Categories"
+              error={error?.message}
+              {...field}
+              data={entries}
+            />
+          );
+        }}
+      />
+    </Box>
+  );
+};
--- src/features/categories/index.ts
+++ src/features/categories/index.ts
@@ -1,2 +1,3 @@
 export * from "./api/getCategories";
 export * from "./mappers/categoryMappers";
+export * from "./components/CategorySelector/CategorySelector";
